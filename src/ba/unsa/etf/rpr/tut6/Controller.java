4ace8eb6fcfe9c0492cfe3d3b7e040db8bb18c2e
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-11-29 02:02:42 +0100

    FXML kontrole u Controller

--- src/ba/unsa/etf/rpr/tut6/Controller.java
+++ src/ba/unsa/etf/rpr/tut6/Controller.java
@@ -1,4 +1,86 @@
 package ba.unsa.etf.rpr.tut6;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+    @FXML
+    public TextField imeField;
+    @FXML
+    public TextField prezimeField;
+    @FXML
+    public TextField brojIndeksaField;
+    @FXML
+    public TextField jmbgField;
+    @FXML
+    public DatePicker dateField;
+    @FXML
+    public ComboBox mjestoBox;
+    @FXML
+    public TextField emailField;
+    @FXML
+    public ChoiceBox odsjekBox;
+    @FXML
+    public ChoiceBox godinaBox;
+    @FXML
+    public ChoiceBox ciklusBox;
+    @FXML
+    public RadioButton redovanBtn;
+    @FXML
+    public RadioButton redSamofinancirajuciBtn;
+    @FXML
+    public CheckBox borackeKategorijeCheck;
+    @FXML
+    public Button potvrdaButton;
+
+    boolean validnoIme, validnoPrezime, validanIndeks, validanJmbg,validanDatum, validanEmail;
+
+    private SimpleStringProperty ime;
+    private SimpleStringProperty prezime;
+    private SimpleStringProperty brojIndeksa;
+    private SimpleStringProperty jmbg;
+    private SimpleStringProperty email;
+
+    public Controller() {
+        ime = new SimpleStringProperty("");
+        prezime = new SimpleStringProperty("");
+        brojIndeksa = new SimpleStringProperty("");
+        jmbg = new SimpleStringProperty("");
+        email = new SimpleStringProperty("");
+    }
+
+    public String getIme() {
+        return ime.get();
+    }
+
+    public String getPrezime() {
+        return prezime.get();
+    }
+
+    public String getBrojIndeksa() {
+        return brojIndeksa.get();
+    }
+
+    public String getJmbg() {
+        return jmbg.get();
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    @FXML
+    public void initialize() {
+        imeField.textProperty().bindBidirectional(ime);
+        prezimeField.textProperty().bindBidirectional(prezime);
+        brojIndeksaField.textProperty().bindBidirectional(brojIndeksa);
+        jmbgField.textProperty().bindBidirectional(jmbg);
+        emailField.textProperty().bindBidirectional(email);
+    }
+
+
+
+
+
 }
